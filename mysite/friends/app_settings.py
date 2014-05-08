e3894256b956aa983b75cd7f5dcf070a70c3643b
freikorps <zhaojinhust@gmail.com>
2014-05-08 15:08:50 -0600

    remove

--- mysite/friends/app_settings.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-
-REDIRECT_FALLBACK_TO_PROFILE = getattr(settings,
-                                       'FRIENDS_REDIRECT_FALLBACK_TO_PROFILE',
-                                       False)
